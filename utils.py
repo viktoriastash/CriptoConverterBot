7607666ca9852383758ccebd627dfc9988950ca4
viktoriastash <89913068+viktoriastash@users.noreply.github.com>
2021-11-21 11:41:46 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,32 @@
+import json
+import requests
+from config import keys
+
+class ConvertionExeption(Exception):
+    pass
+
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionExeption(f"Невозможно перевести одинаковые валюты {base}.")
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionExeption(f"Не удалось обработать валюту {quote}")
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionExeption(f"Не удалось обработать валюту {base}")
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionExeption(f"Не удалось обработать количество {amount}")
+
+        r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}")
+        total_base = json.loads(r.content)[keys[base]] * amount, 2
+
+        return total_base
